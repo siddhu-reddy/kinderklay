30f76292f1573d6c318d048c7c1e7a0f661e817f
Challavivek007 <challavivekreddy24@gmail.com>
2025-02-24 17:29:22 +0530

    updated Nav bar by vivek

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
-import Home from './components/home'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
 
 function App() {
   return (
-    <>
-      <div>
-        hello
-        <Home />
-      </div>
-    </>
-  )
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/admission" element={<h1>Admission Page</h1>} />
+        <Route path="/franchise" element={<h1>Franchise Page</h1>} />
+        <Route path="/branches" element={<h1>Branches Page</h1>} />
+        <Route path="/blog" element={<h1>Blog Page</h1>} />
+        <Route path="/contact" element={<h1>Contact Page</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;

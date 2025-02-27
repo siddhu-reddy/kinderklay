82a94c73fff2c206086908dffb102470d89393ee
siddhu-reddy <aletisiddhureddy@gmail.com>
2025-02-27 11:33:01 +0530

    overlay issue resolved by siddhu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
+import Home from "./components/Home/Home";
+import Admission from "./components/Admission/Admission";
+import Franchise from "./components/Franchise/Franchise";
+import Branches from "./components/Branches/Branches";
+import Blog from "./components/Blog/Blog";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
-    <Router>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/admission" element={<h1>Admission Page</h1>} />
-        <Route path="/franchise" element={<h1>Franchise Page</h1>} />
-        <Route path="/branches" element={<h1>Branches Page</h1>} />
-        <Route path="/blog" element={<h1>Blog Page</h1>} />
-        <Route path="/contact" element={<h1>Contact Page</h1>} />
-      </Routes>
-    </Router>
+    <>
+      
+      <Router>
+        <Nav />
+        <div className="pagecontainer">
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/Admission" element={<Admission/>} />
+            <Route path="/Franchise" element={<Franchise/>} />
+            <Route path="/Branches" element={<Branches/>} />
+            <Route path="/Blog" element={<Blog/>} />
+            <Route path="/Contact" element={<Contact/>} />
+          </Routes>
+        </div>
+        
+      </Router>
+    </>
+    
   );
 }
 
--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,47 +1,39 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
 import "./Nav.css";
 import logo from "../../assets/logo.png";
 
 const Nav = () => {
   return (
-    <nav className="navbarContainer">
-      <div className="logoSection">
-        <img src={logo} alt="Logo" className="logo" />
-      </div>
-      <div className="menuSection">
-        <NavLink exact to="/" activeClassName="activeLink">
-          <h1>Home</h1>
-        </NavLink>
-        <NavLink to="/admission" activeClassName="activeLink">
-          <h1>Admission</h1>
-        </NavLink>
-        <NavLink to="/franchise" activeClassName="activeLink">
-          <h1>Franchise</h1>
-        </NavLink>
-        <NavLink to="/branches" activeClassName="activeLink">
-          <h1>Branches</h1>
-        </NavLink>
-        <NavLink to="/blog" activeClassName="activeLink">
-          <h1>Blog</h1>
-        </NavLink>
-        <NavLink to="/contact" activeClassName="activeLink">
-          <h1>Contact</h1>
-        </NavLink>
-      </div>
-      <div className="socialIcons">
-        <a href="https://facebook.com" target="_blank" rel="noreferrer">
-          <FaFacebookF className="icon" />
-        </a>
-        <a href="https://instagram.com" target="_blank" rel="noreferrer">
-          <FaInstagram className="icon" />
-        </a>
-        <a href="https://youtube.com" target="_blank" rel="noreferrer">
-          <FaYoutube className="icon" />
-        </a>
-      </div>
-    </nav>
+    <div className="navbar">
+            <nav className="navbarContainer">
+        <div className="logoSection">
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
+        <div className="menuSection">
+          <Link to="/" ><h1>Home</h1></Link>
+          <Link to="/Admission" ><h1>Admission</h1></Link>
+          <Link to="/Franchise" ><h1>Franchise</h1></Link>
+          <Link to="/Branches" ><h1>Branches</h1> </Link>
+          <Link to="/Blog" ><h1>Blog</h1></Link>
+          <Link to="/Contact" ><h1>Contact</h1></Link>
+        </div>
+
+        <div className="socialIcons">
+          <a href="https://facebook.com" target="_blank" rel="noreferrer">
+            <FaFacebookF className="icon" />
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noreferrer">
+            <FaInstagram className="icon" />
+          </a>
+          <a href="https://youtube.com" target="_blank" rel="noreferrer">
+            <FaYoutube className="icon" />
+          </a>
+        </div>
+      </nav>
+    </div>
+
   );
 };
 

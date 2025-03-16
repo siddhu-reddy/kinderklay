9d5e01b8201ab4eff6652ced619c481cee0f9beb
siddhu-reddy <aletisiddhureddy@gmail.com>
2025-03-16 17:35:56 +0530

    update css

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -13,6 +13,8 @@ import "./Home.css";
 function Home() {
   return (
   <>
+
+
     <div className='hero'>
         <div className='hero-content'>
             <div className='hero-heading'>
@@ -31,6 +33,8 @@ function Home() {
             
         </div>
     </div>
+
+
     <div className='home1'>
         <div className='home1-content'>
             <div className='home1-heading'>Why Choose<br/>
@@ -42,76 +46,90 @@ function Home() {
         <div className='home1-image' >
             <img src={heroimage} className='home1-img'></img>
         </div>
-
     </div>
+
+
     <div>
-    <div className='home2'>
+        <div className='home2'>
 
-        <div className='home2-image' >
-            <img src={home2image} className='home2-img'></img>
-        </div>
+            <div className='home2-image' >
+                <img src={home2image} className='home2-img'></img>
+            </div>
 
-        <div className='home2-content'>
-            <div className='home2-heading'>Why Early <br/>Education Matters</div>
-            <div className='home2-desc'>Every child is born with a sense of curiosity and wonder. Nurturing these early passions helps shape confident, capable individuals. At KinderKlay, we create a supportive environment where young minds flourish, laying the groundwork for a bright and promising future.</div>
-        </div>
-    </div>
-    </div>
-    <div>
-    <div className='home3'>
-        <img src={wave1} className='home3-img'></img>
-        <div className='home3-content'>
-            <div className='home3-heading'>What We Offer</div>
-            <div className='home3-desc'>The KinderKlay’s mission is to provide affordable, high-quality early education and childcare services for working families to ensure every child.</div>
-            <div className='home3-items'>
-                <div className='home3-item'>
-                    <img src={item1} className='item-img'></img>
-                    <div className='item-content'>
-                        <div className='item-heading'>Art Classes</div>
-                        <div className='item-desc'>Encouraging creativity through painting, drawing, and crafts, helping kids express themselves and build confidence.</div>
-                    </div>
-                </div>
-                <div className='home3-item'>
-                    <img src={item2} className='item-img'></img>
-                    <div className='item-content'>
-                        <div className='item-heading'>Special Education</div>
-                        <div className='item-desc'>Personalized learning support for children with diverse needs, ensuring every child thrives.</div>
-                    </div>
-                </div>
-                <div className='home3-item'>
-                    <img src={item3} className='item-img'></img>
-                    <div className='item-content'>
-                        <div className='item-heading'>Activity Rooms</div>
-                        <div className='item-desc'>Fun-filled spaces with interactive toys and games to enhance teamwork, creativity, and learning.</div>
-                    </div>
-                </div>
-                <div className='home3-item'>
-                    <img src={item4} className='item-img'></img>
-                    <div className='item-content'>
-                        <div className='item-heading'>Languages</div>
-                        <div className='item-desc'>Introducing new languages through songs and stories, making learning fun and engaging.</div>
-                    </div>
-                </div>
-                <div className='home3-item'>
-                    <img src={item5} className='item-img'></img>
-                    <div className='item-content'>
-                        <div className='item-heading'>Birthday Party</div>
-                        <div className='item-desc'>Themed celebrations with fun activities and decorations for a memorable birthday experience.</div>
-                    </div>
-                </div>
-                <div className='home3-item'>
-                    <img src={item6} className='item-img'></img>
-                    <div className='item-content'>
-                        <div className='item-heading'>Healthy Food</div>
-                        <div className='item-desc'>Nutritious, balanced meals made from fresh ingredients to keep kids healthy and energized.</div>
-                    </div>
-                </div>
-                
-
+            <div className='home2-content'>
+                <div className='home2-heading'>Why Early <br/>Education Matters</div>
+                <div className='home2-desc'>Every child is born with a sense of curiosity and wonder. Nurturing these early passions helps shape confident, capable individuals. At KinderKlay, we create a supportive environment where young minds flourish, laying the groundwork for a bright and promising future.</div>
             </div>
         </div>
-        <img src={wave2} className='home3-img'></img>
     </div>
+
+
+    <div>
+        <div className='home3'>
+            <img src={wave1} className='home3-img'></img>
+            <div className='home3-content'>
+                <div className='home3-heading'>What We Offer</div>
+                <div className='home3-desc'>The KinderKlay’s mission is to provide affordable, high-quality early education and childcare services for working families to ensure every child.</div>
+                <div className='home3-items'>
+                    <div className='home3-item'>
+                        <img src={item1} className='h3-item-img'></img>
+                        <div className='h3-item-content'>
+                            <div className='h3-item-heading'>Art Classes</div>
+                            <div className='h3-item-desc'>Encouraging creativity through painting, drawing, and crafts, helping kids express themselves and build confidence.</div>
+                        </div>
+                    </div>
+                    <div className='home3-item'>
+                        <img src={item2} className='h3-item-img'></img>
+                        <div className='h3-item-content'>
+                            <div className='h3-item-heading'>Special Education</div>
+                            <div className='h3-item-desc'>Personalized learning support for children with diverse needs, ensuring every child thrives.</div>
+                        </div>
+                    </div>
+                    <div className='home3-item'>
+                        <img src={item3} className='h3-item-img'></img>
+                        <div className='h3-item-content'>
+                            <div className='h3-item-heading'>Activity Rooms</div>
+                            <div className='h3-item-desc'>Fun-filled spaces with interactive toys and games to enhance teamwork, creativity, and learning.</div>
+                        </div>
+                    </div>
+                    <div className='home3-item'>
+                        <img src={item4} className='h3-item-img'></img>
+                        <div className='h3-item-content'>
+                            <div className='h3-item-heading'>Languages</div>
+                            <div className='h3-item-desc'>Introducing new languages through songs and stories, making learning fun and engaging.</div>
+                        </div>
+                    </div>
+                    <div className='home3-item'>
+                        <img src={item5} className='h3-item-img'></img>
+                        <div className='h3-item-content'>
+                            <div className='h3-item-heading'>Birthday Party</div>
+                            <div className='h3-item-desc'>Themed celebrations with fun activities and decorations for a memorable birthday experience.</div>
+                        </div>
+                    </div>
+                    <div className='home3-item'>
+                        <img src={item6} className='h3-item-img'></img>
+                        <div className='h3-item-content'>
+                            <div className='h3-item-heading'>Healthy Food</div>
+                            <div className='h3-item-desc'>Nutritious, balanced meals made from fresh ingredients to keep kids healthy and energized.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <img src={wave2} className='home3-img'></img>
+        </div>
+
+
+        <div className='home4'>
+            <div className='home4-content'>
+                <div className='home4-heading'></div>
+                <div className='home4-desc'></div>
+                <div className='home4-items'>
+                    <div className='home4-item'>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
     
     </div>
   </>
